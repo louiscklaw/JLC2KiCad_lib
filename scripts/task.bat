a3b9be409d18776d4064e065c4ab5db26b8edb71
louiscklaw <logickee@gmail.com>
2022-02-08 18:36:53 +0800

    update,

--- scripts/task.bat
+++ scripts/task.bat
@@ -5,7 +5,7 @@ mkdir C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty
 mkdir C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty\packages3d
 
 pipenv run python JLC2KiCad_lib.py ^
-  C97643 ^
+  C191736 ^
   -dir My_lib 
 
 copy C:\Users\logic\_TODO\JLC2KiCad_lib\My_lib\footprint C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty\
