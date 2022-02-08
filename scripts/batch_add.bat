6cd7442f9b8ced065e6626121cc6195713c3068b
louiscklaw <logickee@gmail.com>
2022-02-08 22:05:57 +0800

    update,

--- scripts/batch_add.bat
+++ scripts/batch_add.bat
@@ -6,11 +6,11 @@ rmdir  /S /Q C:\Users\logic\_TODO\JLC2KiCad_lib\My_lib
 mkdir C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty
 mkdir C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty\packages3d
 
-pipenv run python JLC2KiCad_lib.py ^
-  %1 ^
-  -dir My_lib 
+pipenv run python JLC2KiCad_lib.py %1 -dir My_lib 
 
 pipenv run python check_footprint_pad.py My_lib\footprint
+pipenv run python update_descr.py My_lib\footprint %4 %5
+pipenv run python update_tags.py My_lib\footprint %2 %3
 
 
 copy C:\Users\logic\_TODO\JLC2KiCad_lib\My_lib\footprint C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty\
