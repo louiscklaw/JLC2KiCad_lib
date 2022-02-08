59529c772391eb4c8fb754c4713648515ed26402
louiscklaw <logickee@gmail.com>
2022-02-08 19:07:05 +0800

    update,

--- scripts/task.bat
+++ scripts/task.bat
@@ -8,5 +8,8 @@ pipenv run python JLC2KiCad_lib.py ^
   C191736 ^
   -dir My_lib 
 
+pipenv run python check_footprint_pad.py My_lib\footprint
+
+
 copy C:\Users\logic\_TODO\JLC2KiCad_lib\My_lib\footprint C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty\
 copy C:\Users\logic\_TODO\JLC2KiCad_lib\My_lib\footprint\packages3d C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty\packages3d\
