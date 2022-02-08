f7661e29ea04d5f9f3b9b90f87f3ae769a38cbaa
louiscklaw <logickee@gmail.com>
2022-02-08 18:17:20 +0800

    update,

---
+++ scripts/task.bat
@@ -0,0 +1,12 @@
+del /S /Q C:\Users\logic\_TODO\JLC2KiCad_lib\My_lib
+rmdir  /S /Q C:\Users\logic\_TODO\JLC2KiCad_lib\My_lib
+
+mkdir C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty
+mkdir C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty\packages3d
+
+pipenv run python JLC2KiCad_lib.py ^
+  C97643 ^
+  -dir My_lib 
+
+copy C:\Users\logic\_TODO\JLC2KiCad_lib\My_lib\footprint C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty\
+copy C:\Users\logic\_TODO\JLC2KiCad_lib\My_lib\footprint\packages3d C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty\packages3d\
