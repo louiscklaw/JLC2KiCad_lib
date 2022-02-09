bf9378b05905a74830b30eab7c177b97eababc23
louiscklaw <logickee@gmail.com>
2022-02-09 08:52:17 +0800

    update,

--- scripts/batch_add.bat
+++ scripts/batch_add.bat
@@ -1,17 +1,23 @@
 @echo on
 
-del /S /Q C:\Users\logic\_TODO\JLC2KiCad_lib\My_lib
-rmdir  /S /Q C:\Users\logic\_TODO\JLC2KiCad_lib\My_lib
+set TMP_FOOTPRINT_BASE_DIR=tmp\%1
+set TMP_FOOTPRINT_DIR=%TMP_FOOTPRINT_BASE_DIR%\footprint
+
+del /S /Q C:\Users\logic\_TODO\JLC2KiCad_lib\%TMP_FOOTPRINT_DIR%
+rmdir  /S /Q C:\Users\logic\_TODO\JLC2KiCad_lib\%TMP_FOOTPRINT_DIR%
+
+mkdir %TMP_FOOTPRINT_DIR%
 
 mkdir C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty
 mkdir C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty\packages3d
 
-pipenv run python JLC2KiCad_lib.py %1 -dir My_lib 
+pipenv run python JLC2KiCad_lib.py %2 -dir %TMP_FOOTPRINT_BASE_DIR% 
 
-pipenv run python check_footprint_pad.py My_lib\footprint
-pipenv run python update_descr.py My_lib\footprint %4 %5
-pipenv run python update_tags.py My_lib\footprint %2 %3
+pipenv run python check_footprint_pad.py %TMP_FOOTPRINT_DIR%
+pipenv run python update_package_3d_path.py %TMP_FOOTPRINT_DIR%
 
+pipenv run python update_descr.py %TMP_FOOTPRINT_DIR% %5 %6
+pipenv run python update_tags.py %TMP_FOOTPRINT_DIR% %3 %4
 
-copy C:\Users\logic\_TODO\JLC2KiCad_lib\My_lib\footprint C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty\
-copy C:\Users\logic\_TODO\JLC2KiCad_lib\My_lib\footprint\packages3d C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty\packages3d\
+copy C:\Users\logic\_TODO\JLC2KiCad_lib\%TMP_FOOTPRINT_DIR% C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty\
+copy C:\Users\logic\_TODO\JLC2KiCad_lib\%TMP_FOOTPRINT_DIR%\packages3d C:\Users\logic\_workspace\kicad-playlist\library\footprint\LCSC.pretty\packages3d\
